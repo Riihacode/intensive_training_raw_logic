45365f2f31cf427574a122d87cea8167a71092b0
Riihacode <riihacode@gmail.com>
2025-12-25 23:49:59 +0700

    [KOTLIN_LANGUAGE] update  04_part1_mengenal_tipe_data_angka_lebih_dalam.kt

---
+++ raw_logic_kotlin/raw_logic_kotlin/src/main/kotlin/part_01_tipe_data/04_part1_mengenal_tipe_data_angka_lebih_dalam.kt
@@ -0,0 +1,22 @@
+package org.example.part1_01_tipe_data
+
+fun main() {
+  val angka1 = 20
+  val angka2 = 5
+
+  //penjumlahan
+  val hasilPenjumlahan = angka1 + angka2
+  println(hasilPenjumlahan)
+
+  val hasilPengurangan = angka1 - angka2
+  println(hasilPengurangan)
+
+  val hasilPembagian = angka1 / angka2
+  println(hasilPembagian)
+
+  val hasilPerkalian = angka1 * angka2
+  println(hasilPerkalian)
+
+  val hasilPembagianPecahan = angka1 % angka2
+  println(hasilPembagianPecahan)
+}
