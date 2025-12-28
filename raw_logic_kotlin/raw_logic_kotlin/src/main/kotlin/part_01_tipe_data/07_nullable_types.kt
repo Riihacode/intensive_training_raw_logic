62c5ae936fb78bd10ebc28625ec1d8327c20ad6e
Riihacode <riihacode@gmail.com>
2025-12-28 23:19:49 +0700

    [KOTLIN_LANGUAGE] updated 07_nullable_types

--- raw_logic_kotlin/raw_logic_kotlin/src/main/kotlin/part_01_tipe_data/07_nullable_types.kt
+++ raw_logic_kotlin/raw_logic_kotlin/src/main/kotlin/part_01_tipe_data/07_nullable_types.kt
@@ -2,6 +2,7 @@ package com.example.part_01_tipe_data
 
 fun main() {
   // SAFE CALLS OPERATOR
+  println("================================")
   val judul: String = "Belajar Kotlin" 
   var catatanBuku: String? = null
   var panjangCatatanBuku: Int? = catatanBuku?.length
@@ -10,4 +11,11 @@ fun main() {
   catatanBuku = "GACOR WOY"
   panjangCatatanBuku = catatanBuku?.length
   println("Buku $judul memiliki panjang catatan buku sebanyak $panjangCatatanBuku")
+
+  // Elvis operator
+  println("===========================")
+  val judul2: String = "Belajar kotlin language"
+  var noteBuku: String? = null
+  var panjangNoteBuku: Int? = noteBuku?.length ?: 0
+  println("Buku berjudul \"$judul2\", memiliki panjang Note Buku yaitu $panjangNoteBuku")
 }
