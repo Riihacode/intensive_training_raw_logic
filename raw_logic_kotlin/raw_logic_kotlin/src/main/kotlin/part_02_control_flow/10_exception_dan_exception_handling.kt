1f30b2018612b3322d2dcaa3eb3b55576e6bae20
Riihacode <riihacode@gmail.com>
2026-01-08 00:34:12 +0700

    [KOTLIN_LANGUAGE] updated 10_exception_dan_exception_handling.kt

--- raw_logic_kotlin/raw_logic_kotlin/src/main/kotlin/part_02_control_flow/10_exception_dan_exception_handling.kt
+++ raw_logic_kotlin/raw_logic_kotlin/src/main/kotlin/part_02_control_flow/10_exception_dan_exception_handling.kt
@@ -6,7 +6,12 @@ fun main() {
    *  untuk atasi program error tidak berhenti mendadak
    */
 
-  // Arithmetic exception error
+  println("===========================================")
+  println(" [EXCEPTION] ")
+  /* 
+   * Arithmetic exception error
+   *
+   */
   /*
   val errorValueA: Int = 6
   val errorValueB: Int = 0
@@ -23,7 +28,60 @@ fun main() {
 
   println("\n=========================================")
   // NulPointerException
+  /*
   val NPEValueA: String? = null
   val NPEValueB: String = NPEValueA!!
   println(NPEValueB)
+  */
+
+  println("===========================================")
+  println(" [EXCEPTION-HANDLING]")
+  // blok try-catch
+  /*
+  val valueExHandl: String? = null
+  try {
+    val valueExHandlError: String = valueExHandl!!
+    println("valueHandlError")
+  } catch (e: NullPointerException) {
+    println("Nilai valueExHandl adalah null tapi dipaksa kalau tidak boleh null. Error: $e")
+  }
+  */
+
+  println("============================================")
+  // blok finally (opsional)
+  /*
+  val valueXFinallyA: String = "11.0"
+  try {
+    val valueXFinallyB: Int = valueXFinallyA.toInt()
+    println(valueXFinallyB)
+  } catch (e: NullPointerException) { // println tidak akan dieksekusi karena ini jenis NumberFormatException
+    println("Error NullPointerException pada valueXFinallyB karena originalnya adalah 11.0. Error: $e")
+  } finally {
+    println("Program logout dan membersihkan memory")   // akan berhenti di sini karena crash-nya ada di sini
+  }
+
+  println("Proses selanjutnya akan dieksekusi ...")
+  */
+  
+  println("============================================")
+  // blok finally (opsional)
+  val valueMultiCatchFinallyA: String = "11.0"
+  try {
+    val valueMultiCatchFinallyB: Int = valueMultiCatchFinallyA.toInt()
+    println(valueMultiCatchFinallyB)
+  } catch (e: NullPointerException) { // println tidak akan dieksekusi karena ini jenis NumberFormatException
+    println("Error NullPointerException pada valueXFinallyB karena originalnya adalah 11.0. Error: $e")
+  } catch (e: ArithmeticException) {
+    println("Error ArithmeticException woy!! Error = $e")
+  } catch (e: NumberFormatException) {
+    println("[BENAR] Tebakan coding manual anda sebelum compile telah benar. Error = $e")
+  } 
+  finally {
+    println("[Multi Catch Finally] Program logout dan membersihkan memory")   // akan berhenti di sini karena crash-nya ada di sini
+  }
+
+  println("Proses selanjutnya akan dieksekusi ...")
+
+  println("===================================================================")
+  println("[IMPORTANT] Ini hanya sebagian, dokumentasi EXCEPTION yang lengkap di sini \"https://kotlinlang.org/docs/exceptions.html\"")
 }
