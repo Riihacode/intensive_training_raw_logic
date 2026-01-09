30046bed0a526be708da4dc18c3aceef7e6bc9f9
Riihacode <riihacode@gmail.com>
2026-01-09 23:14:45 +0700

    [KOTLIN_LANGUAGE] updated 10_exception_dan_exception_handling_kotlin_docs.kt

---
+++ raw_logic_kotlin/raw_logic_kotlin/src/main/kotlin/part_02_control_flow/10_exception_dan_exception_handling_kotlin_docs.kt
@@ -0,0 +1,13 @@
+package com.example.part_02_control_flow
+
+fun main() {
+  // arithmethic exception 
+  // numberformat exception
+  // null pointer exception
+
+  // throw exception
+  /* Throwing an exception indicates that an unexpected runtime error has occurred in the code. 
+   * Exceptions are objects, and throwing one creates an instance of an exception class
+   */
+  throw IllegalArgumentException()
+}
