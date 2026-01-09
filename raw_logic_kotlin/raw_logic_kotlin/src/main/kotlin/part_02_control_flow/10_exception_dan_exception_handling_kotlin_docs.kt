60cd800a827b4e5e0a196c0d9627025386d8821a
Riihacode <riihacode@gmail.com>
2026-01-10 00:15:05 +0700

    [KOTLIN_LANGUAGE] updated 10_exception_dan_exception_handling_kotlin_docs.kt

--- raw_logic_kotlin/raw_logic_kotlin/src/main/kotlin/part_02_control_flow/10_exception_dan_exception_handling_kotlin_docs.kt
+++ raw_logic_kotlin/raw_logic_kotlin/src/main/kotlin/part_02_control_flow/10_exception_dan_exception_handling_kotlin_docs.kt
@@ -8,6 +8,14 @@ fun main() {
   // throw exception
   /* Throwing an exception indicates that an unexpected runtime error has occurred in the code. 
    * Exceptions are objects, and throwing one creates an instance of an exception class
+   * bisa langsung sperti ini di fun main():
+   *    throw IllegalArgumentException()
    */
-  throw IllegalArgumentException()
+  //throw IllegalArgumentException()
+  val userInput: Int = -1
+  //val cause: String = IllegalArgumentException("Original cause: illegal state")
+  val cause: IllegalArgumentException = IllegalArgumentException("Original cause: illegal state")
+  if(userInput < 1) {
+    throw IllegalArgumentException("Input must be non negative", cause)
+  }
 }
